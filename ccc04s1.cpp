0ad67b1119db42ed8c6e25b64cca46f5ac7da428
do_ur_homwork <109537113+4011v20c@users.noreply.github.com>
2026-01-30 16:53:50 -0500

    Add files via upload

--- ccc04s1.cpp
+++ ccc04s1.cpp
@@ -1,27 +1,127 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+int main() {
+    cin.sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    cin>>n;
+    for(int i = 0; i<n; i++){
+        bool valid = true;
 
-int main(){
-    int a, b, height; cin >> a >> b >> height;
-    for(int i = 0; i < a; i++){
-        for(int i = 0; i < 3; i++){
-            cout << "*";
-            if (i < 2){
-                for(int i = 0; i < b; i++){
-                    cout << " ";
-                }
+        string a,b,c;
+        cin>>a>>b>>c;
+        int x,y,z;
+        x = a.length();
+        y = b.length();
+        z = c.length();
+
+        string p1, p2, p3;
+        //prefix
+        p1 = a.substr(0, y);
+        if(p1 == b){
+            valid = false;
+                     cout<<"No"<<"\n";
+
+            continue;
+        }
+        p1 = a.substr(0, z);
+        if(p1==c){
+            valid = false;
+                     cout<<"No"<<"\n";
+
+            continue;
+        }
+        p2 = b.substr(0, x);
+        if(p2 == a){
+            valid = false;
+                     cout<<"No"<<"\n";
+
+            continue;
+        }
+        p2 = b.substr(0, z);
+        if(p2==c){
+            valid = false;
+                     cout<<"No"<<"\n";
+
+            continue;
+        }
+
+        p3 = c.substr(0, x);
+        if(p3 == a){
+            valid = false;
+                     cout<<"No"<<"\n";
+
+            continue;
+        }
+        p3 = c.substr(0, y);
+        if(p3==b){
+            valid = false;
+                     cout<<"No"<<"\n";
+
+            continue;
+        }
+
+         //suffix
+         if(x>y){
+        p1 = a.substr(x-y, y);
+        if(p1 == b){
+            valid = false;
+                     cout<<"No"<<"\n";
+
+            continue;
+        }
+         }
+          if(x>z){
+        p1 = a.substr(x-z, z);
+        if(p1==c){
+            valid = false;
+                     cout<<"No"<<"\n";
+
+            continue;
+        }
+          }
+           if(y>x){
+        p2 = b.substr(y-x, x);
+        if(p2 == a){
+            valid = false;
+                     cout<<"No"<<"\n";
+
+            continue;
+        }
+           }
+            if(y>z){
+        p2 = b.substr(y-z, z);
+        if(p2==c){
+            valid = false;
+                     cout<<"No"<<"\n";
+
+            continue;
+        }
             }
+
+             if(z>x){
+        p3 = c.substr(z-x, x);
+        if(p3 == a){
+            valid = false;
+                     cout<<"No"<<"\n";
+
+            continue;
         }
-        cout << "\n";
-    }
-    for(int i = 0; i < b*2+3; i++){
-        cout << "*";
-    }
-    cout << "\n";
-    for(int i = 0; i < height; i++){
-        for(int i = 0; i <= b; i++){
-            cout << " ";
+             }
+
+              if(z>y){
+        p3 = c.substr(z-y, y);
+        if(p3==b){
+            valid = false;
+                     cout<<"No"<<"\n";
+
+            continue;
         }
-        cout << "*" << "\n";
-    }
-}
+              }
+         if(valid == true){
+         cout<<"Yes"<<"\n";
+     }
+
+     }
+
+ }
